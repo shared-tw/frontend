0bf7b2106632928d5374c09607cbd9deb2870184
jason06286 <dj4871114@gmail.com>
2021-06-17 18:35:24 +0800

    update AppButton from ConfirmModel.vue

--- src/components/organisms/ConfirmModel.vue
+++ src/components/organisms/ConfirmModel.vue
@@ -8,16 +8,16 @@ const isOpen = ref(false)
     <div v-if="isOpen" class="relative w-full h-full flex justify-center items-center">
       <div class="bg-black opacity-50 fixed inset-0 " @click="isOpen=false" />
       <div
-        class=" sm:w-5/6 sm:mx-auto  w-full  p-6 bg-white text-darkblue z-10 flex  justify-center items-center  flex-col rounded  font-sans font-normal"
+        class="sm:w-5/6 sm:mx-auto w-full p-6 bg-white text-darkblue z-10 flex justify-center items-center flex-col rounded  font-sans font-normal"
       >
-        <h3 class="text-center text-xl  font-bold mb-4 relative w-full">
+        <h3 class="text-center text-xl font-bold mb-4 relative w-full">
           確認送出
           <span class="absolute right-0 text-base" @click="isOpen=false"><mdi:close /></span>
         </h3>
         <p>
           確認後，我們會將您的聯絡資訊提供給需求方，以利雙方聯繫
         </p>
-        <div class="mt-9 w-full flex align-center sm:w-auto ">
+        <div class="sm:w-auto mt-9 w-full flex align-center">
           <FormCheckbox
             name="noShow"
             :value="false"
@@ -25,7 +25,7 @@ const isOpen = ref(false)
           />
         </div>
         <div class="flex justify-end w-full ">
-          <AppButton class="btn-outline text-gray-400 border-gray-300  hover:bg-gray-300 hover:text-white ">
+          <AppButton outline class="text-gray-400 border-gray-300  hover:bg-gray-300 hover:text-white ">
             取消
           </AppButton>
           <AppButton class="bg-darkblue ml-3 hover:bg-blue-900 ">
