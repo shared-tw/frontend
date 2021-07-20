7ced7d5a366d5350cf0035447865936785e77307
hanlee <me@hanlee.co>
2021-07-20 22:36:00 +0900

    feat: handle oauth callback redirect

--- src/pages/oauth/interaction.vue
+++ src/pages/oauth/interaction.vue
@@ -1,3 +1,24 @@
+<script setup lang="ts">
+import { useRoute, useRouter } from 'vue-router'
+import authStore from '@/store/auth'
+
+const router = useRouter()
+const route = useRoute()
+
+if (route.query.token) {
+  if (typeof route.query.token === 'string') {
+    authStore.state.token = route.query.token
+  }
+}
+
+if (route.query.unregistered) {
+  router.push('/register/donator')
+} else {
+  router.push('/')
+}
+
+</script>
+
 <template>
   <h1>
     Success
