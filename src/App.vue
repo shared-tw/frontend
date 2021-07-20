2ed248355d86b22a323ef9e874b9953dc97693ec
hanlee <me@hanlee.co>
2021-07-21 05:10:00 +0900

    chore: remove refresh token in open web page

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,3 @@
-<script setup lang="ts">
-import { onMounted } from 'vue'
-import authStore from '@/store/auth'
-
-onMounted(async() => {
-  await authStore.actions.refreshToken()
-})
-</script>
-
 <template>
   <TheHeader v-show="!$route.params.org" />
   <router-view />
