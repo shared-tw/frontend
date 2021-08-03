c7f45b7a1dfe6293a56e6d3ffdd2f301ab11d4b4
hanlee <me@hanlee.co>
2021-08-03 22:05:16 +0900

    feat: add api call to get user info

--- src/components/organisms/user/UserCard.vue
+++ src/components/organisms/user/UserCard.vue
@@ -1,17 +1,24 @@
 <script setup lang="ts">
+import userStore from '@/store/user'
+import { onMounted, computed } from 'vue'
 
+onMounted(async() => {
+  await userStore.actions.getUser()
+})
+
+const user = computed(() => userStore.state.user)
 </script>
 
 <template>
   <div class="flex py-4 px-5 bg-primary-lightest border border-primary-medium rounded-md">
     <div class="rounded-full bg-primary h-8 w-8 flex items-center justify-center">
       <span class="text-lg font-medium text-white">
-        S
+        {{ user?.name.slice(0, 1) }}
       </span>
     </div>
     <div class="mx-4 flex flex-col">
-      <span class="text-lg font-bold">Stacy Shih</span>
-      <span class="text-sm">＠0968123456</span>
+      <span class="text-lg font-bold">{{ user?.name }}</span>
+      <span class="text-sm">{{ user?.email }}</span>
     </div>
     <div class="ml-auto">
       <CheckAllButton to="/" />
