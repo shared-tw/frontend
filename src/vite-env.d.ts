c4406b5d6a5015601602d09a9ed32b734f096515
hanlee <me@hanlee.co>
2021-09-01 23:50:48 +0900

    fix: remove unused type

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,6 +1,5 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-pages/client" />
-/// <reference types="vite-plugin-icons/client" />
 
 interface ImportMetaEnv {
   VITE_API_ENDPOINT: string
