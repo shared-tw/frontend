f5673d4682aa80c361813024c62c74fe04fef594
hanlee <me@hanlee.co>
2021-09-03 21:29:58 +0900

    chore: update deps, ts 4.4.2, fix icon types.

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,5 +1,6 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-pages/client" />
+/// <reference types="unplugin-icons/types/vue3" />
 
 interface ImportMetaEnv {
   VITE_API_ENDPOINT: string
